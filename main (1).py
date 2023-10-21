f8bb7de9b46f5f7cdd07b4ea310889ef78bf09a6
manikandan2005vcsm <146510457+manikandan2005vcsm@users.noreply.github.com>
2023-10-21 16:11:02 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,13 +1,16 @@
-def CheckLeap(Year):  
-  
-  if((Year % 400 == 0) or  
-     (Year % 100 != 0) and  
-     (Year % 4 == 0)):   
-    print("Given Year is a leap Year");  
-  
-  else:  
-    print ("Given Year is not a leap Year")  
-  
-Year = int(input("Enter the number: "))  
- 
-CheckLeap(Year)
+class Player:
+  def play(self):
+    print("The player is playing cricket")
+
+class Batsman(Player):
+  def play(self):
+    print("The Batsman is Batting")
+
+class Bowler(Player):
+  def play(self):
+    print("The bowler is bowling")
+
+batsman=Batsman()
+bowler=Bowler()
+batsman.play()
+bowler.play()
--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-def factorial(x):
-  """This is a recursive function
-    to find the factorial of an integer"""
+class BankAccount:
+  def __init__(self,account_number,account_holder_name,initial_balance=0.0):
+    self.__account_number=account_number
+    self.__account_holder_name=account_holder_name
+    self.__account_balance=initial_balance
 
-  if x == 1:
-    return 1
-  else:
+  def deposit(self,amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited Rs{}. New Balance:Rs{}".format(amount,self.__account_balance ))
+    else:
+      print("Invalid deposit amount. Please deposit a positive amount.")
+  def withdraw(self,amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("Withdraw Rs{}. New balance: Rs {} ".format(amount,self.__account_balance))
+  
+    else:
+      print("Invalid withdarwal amount or insufficient balnce.")
 
-    return (x * factorial(x - 1))
+  def display_balance(self):
+    print("Account balance for {} (Account #{}): Rs{}".format(self.__account_holder_name, self.__account_number, self.__account_balance))
 
 
-num = 7
 
-result = factorial(num)
-print("The factorial of", num, "is", result)
+account = BankAccount(account_number="1234567891",account_holder_name="Tony Stark",initial_balance=500000.0)
+
+
+account.display_balance()
+account.deposit(5000000000.0)
+account.withdraw(100000)
+account.display_balance()
