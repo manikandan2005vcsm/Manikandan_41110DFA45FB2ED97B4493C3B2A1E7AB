b63e0cbc91d2c30874516058000d988985b5a89a
manikandan2005vcsm <146510457+manikandan2005vcsm@users.noreply.github.com>
2023-10-21 17:00:23 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,16 +1,25 @@
-class Player:
-  def play(self):
-    print("The player is playing cricket")
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+      self.name=name
+      self.roll_number=roll_number
+      self.cgpa=cgpa
 
-class Batsman(Player):
-  def play(self):
-    print("The Batsman is Batting")
+def sort_students(student_list):
 
-class Bowler(Player):
-  def play(self):
-    print("The bowler is bowling")
+  sorted_students = sorted(student_list,
+                          key =lambda student:student.cgpa,reverse=True)
+  return sorted_students
+    
+    
+students = [
+    Student("suriya","cb45",10.0),
+    Student("abhishek","cb46",9.9),
+    Student("yogesh","cb47",9.5),
+    Student("sanjay","cb48",8.5)]
 
-batsman=Batsman()
-bowler=Bowler()
-batsman.play()
-bowler.play()
+
+sorted_students=sort_students(students)
+   
+   
+for student in sorted_students:
+    print("Name: {},Roll Number: {},CGPA: {}".format(student.name, student.roll_number,student.cgpa))
