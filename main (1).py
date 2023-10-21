ec990b68b3a638f79543e0b1db2d00a5432781f0
manikandan2005vcsm <146510457+manikandan2005vcsm@users.noreply.github.com>
2023-10-21 16:59:20 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,25 +1,16 @@
-class Student:
-    def __init__(self, name, roll_number, cgpa):
-      self.name=name
-      self.roll_number=roll_number
-      self.cgpa=cgpa
+class Player:
+  def play(self):
+    print("The player is playing cricket")
 
-def sort_students(student_list):
+class Batsman(Player):
+  def play(self):
+    print("The Batsman is Batting")
 
-  sorted_students = sorted(student_list,
-                          key =lambda student:student.cgpa,reverse=True)
-  return sorted_students
-    
-    
-students = [
-    Student("suriya","cb45",10.0),
-    Student("abhishek","cb46",9.9),
-    Student("yogesh","cb47",9.5),
-    Student("sanjay","cb48",8.5)]
+class Bowler(Player):
+  def play(self):
+    print("The bowler is bowling")
 
-
-sorted_students=sort_students(students)
-   
-   
-for student in sorted_students:
-    print("Name: {},Roll Number: {},CGPA: {}".format(student.name, student.roll_number,student.cgpa))
+batsman=Batsman()
+bowler=Bowler()
+batsman.play()
+bowler.play()
--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-def linearsearchproduct(productlist, targetproduct):
-  indicates=[]
+class BankAccount:
+  def __init__(self,account_number,account_holder_name,initial_balance=0.0):
+    self.__account_number=account_number
+    self.__account_holder_name=account_holder_name
+    self.__account_balance=initial_balance
 
-  for index, product in enumerate(productlist):
-    if product==targetproduct:
-      indicates.append(index)
-  return indicates
+  def deposit(self,amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited Rs{}. New Balance:Rs{}".format(amount,self.__account_balance ))
+    else:
+      print("Invalid deposit amount. Please deposit a positive amount.")
+  def withdraw(self,amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("Withdraw Rs{}. New balance: Rs {} ".format(amount,self.__account_balance))
+  
+    else:
+      print("Invalid withdarwal amount or insufficient balnce.")
 
-#example 
-products=["shoes","boot","loafer", "shoes","sandal","shoes"]
-target="shoes"
-target2='apple'
-result=linearsearchproduct(products, target)
-print(result)
+  def display_balance(self):
+    print("Account balance for {} (Account #{}): Rs{}".format(self.__account_holder_name, self.__account_number, self.__account_balance))
+
+
+
+account = BankAccount(account_number="1234567891",account_holder_name="Tony Stark",initial_balance=500000.0)
+
+
+account.display_balance()
+account.deposit(5000000000.0)
+account.withdraw(100000)
+account.display_balance()
